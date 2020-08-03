082910715df1faa8331b235493802f4756f830ca
Tree-Nulbo <exopas58@gmail.com>
2020-08-03 16:09:43 +0900

    csv to json file
    You can now convert csv file to json file. This python code is used when you want to upload JSON file to framer.

---
+++ csv_to_json.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import json
+
+# read csv file and convert it to dictionary
+df = pd.read_csv(r'C:\Users\cross\Documents\GitHub\prehension\csv_data\10sec_sensor_fix.csv')
+date = df["Date"].tolist()
+nop = df["NoP"].tolist()
+temp_dict = {"Date":date, "Time":time, "NoP":nop}
+
+# write dictionary to JSON file
+with open(r'C:\Users\cross\Documents\GitHub\prehension\csv_data\temp.json', 'w', encoding='utf-8') as fp:
+    json.dump(temp_dict, fp)
